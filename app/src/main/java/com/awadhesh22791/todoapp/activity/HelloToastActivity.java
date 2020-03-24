9bf49548fe4df7113bfcd8dc258757538a9b4a62
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-24 23:03:48 +0530

    Added button click handler.

--- app/src/main/java/com/awadhesh22791/todoapp/activity/HelloToastActivity.java
+++ app/src/main/java/com/awadhesh22791/todoapp/activity/HelloToastActivity.java
@@ -3,14 +3,31 @@ package com.awadhesh22791.todoapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.awadhesh22791.todoapp.R;
 
 public class HelloToastActivity extends AppCompatActivity {
 
+    private int mCount=0;
+    TextView mShowCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_toast);
+        mShowCount=(TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if(mShowCount!=null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
