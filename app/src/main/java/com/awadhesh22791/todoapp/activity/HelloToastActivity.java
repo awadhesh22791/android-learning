f9533a0128248b8f6a73fe76f92039476ac0c465
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-24 22:53:47 +0530

    Added new layout for Hello Toast

---
+++ app/src/main/java/com/awadhesh22791/todoapp/activity/HelloToastActivity.java
@@ -0,0 +1,16 @@
+package com.awadhesh22791.todoapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.awadhesh22791.todoapp.R;
+
+public class HelloToastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_toast);
+    }
+}
