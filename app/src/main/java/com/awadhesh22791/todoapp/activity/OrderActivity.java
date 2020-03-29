9b4383490bc9a0f895d04ed8bcb9c84a3d8ae9c1
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-29 22:53:26 +0530

    Added Checkboxes for customer delights.

--- app/src/main/java/com/awadhesh22791/todoapp/activity/OrderActivity.java
+++ app/src/main/java/com/awadhesh22791/todoapp/activity/OrderActivity.java
@@ -1,20 +1,37 @@
 package com.awadhesh22791.todoapp.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.awadhesh22791.todoapp.R;
 import com.awadhesh22791.todoapp.constant.Field;
 
-public class OrderActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private String TAG="OrderActivity";
     private TextView orderMessage;
+    private EditText editTextPhone;
     private RadioButton radioButtonDeliveryMethod;
+    private Spinner spinnerPhoneType;
+    private List<String>additional=new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +48,44 @@ public class OrderActivity extends AppCompatActivity {
             orderMessage.setText("Please order something.");
         }
         radioButtonDeliveryMethod=findViewById(R.id.radio_button_delivery_method_same_day);
-        radioButtonDeliveryMethod.setChecked(true);
+        if(radioButtonDeliveryMethod!=null)
+            radioButtonDeliveryMethod.setChecked(true);
+        spinnerPhoneType=findViewById(R.id.spinner_phone_type);
+        if(spinnerPhoneType!=null){
+            spinnerPhoneType.setOnItemSelectedListener(this);
+            ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.phone_type,android.R.layout.simple_spinner_item);
+            spinnerPhoneType.setAdapter(adapter);
+        }
+        editTextPhone=findViewById(R.id.edit_text_phone);
+        if(editTextPhone!=null){
+            editTextPhone.setOnEditorActionListener(editorActionListener);
+        }
+    }
+
+    private TextView.OnEditorActionListener editorActionListener=new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            boolean handled=false;
+            if(actionId== EditorInfo.IME_ACTION_SEND){
+                dailNumber();
+                handled=true;
+            }
+            return handled;
+        }
+    };
+
+    private void dailNumber(){
+        if(editTextPhone!=null){
+            String phoneNumber="tel:"+editTextPhone.getText().toString();
+            Log.i(TAG,"Dialing "+editTextPhone.getText().toString());
+            Intent intent=new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(phoneNumber));
+            if(intent.resolveActivity(getPackageManager())!=null){
+                startActivity(intent);
+            }else{
+                Log.d(TAG,"Can't handle dial phone number.");
+            }
+        }
     }
 
     public void selectDeliveryMethod(View view) {
@@ -51,5 +105,29 @@ public class OrderActivity extends AppCompatActivity {
     }
     public void displayMessage(String message){
         Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String spinnerLabel=adapterView.getItemAtPosition(position).toString();
+        displayMessage(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void addAdditional(View view) {
+        CheckBox delight=((CheckBox)view);
+        if(delight!=null) {
+            boolean isAdditionalSelected = ((CheckBox) view).isChecked();
+            if (isAdditionalSelected) {
+                additional.add(delight.getText().toString());
+            } else {
+                additional.remove(delight.getText().toString());
+            }
+            Toast.makeText(getApplicationContext(),"Selected delight: "+additional.toString(),Toast.LENGTH_SHORT).show();
+        }
     }
 }
