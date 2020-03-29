d967f0223060f517ad8470542e7b376361710272
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-29 09:15:10 +0530

    Added text scrolling view.

---
+++ app/src/main/java/com/awadhesh22791/todoapp/TextScrollingViewActivity.java
@@ -0,0 +1,14 @@
+package com.awadhesh22791.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextScrollingViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_scrolling_view);
+    }
+}
