1a3640658086e1545fcf83bf9247091acc1f647a
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-29 18:40:44 +0530

    Added activity and text view.

---
+++ app/src/main/java/com/awadhesh22791/todoapp/ClickableImageActivity.java
@@ -0,0 +1,14 @@
+package com.awadhesh22791.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickableImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clickable_image);
+    }
+}
