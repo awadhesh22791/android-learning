a90228de134b9336825dcae254dc4a710844f49b
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-29 21:04:34 +0530

    Added input fields.

--- app/src/main/java/com/awadhesh22791/todoapp/activity/OrderActivity.java
+++ app/src/main/java/com/awadhesh22791/todoapp/activity/OrderActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.awadhesh22791.todoapp.R;
 import com.awadhesh22791.todoapp.constant.Field;
 
 public class OrderActivity extends AppCompatActivity {
     private TextView orderMessage;
+    private RadioButton radioButtonDeliveryMethod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +30,26 @@ public class OrderActivity extends AppCompatActivity {
         }else{
             orderMessage.setText("Please order something.");
         }
+        radioButtonDeliveryMethod=findViewById(R.id.radio_button_delivery_method_same_day);
+        radioButtonDeliveryMethod.setChecked(true);
+    }
+
+    public void selectDeliveryMethod(View view) {
+        boolean checked=((RadioButton)view).isChecked();
+        if(checked) {
+            switch (view.getId()) {
+                case R.id.radio_button_delivery_method_same_day:
+                    displayMessage(getString(R.string.delivery_method_same_day));
+                    break;
+                case R.id.radio_button_delivery_method_next_day:
+                    displayMessage(getString(R.string.delivery_method_next_day));
+                    break;
+                case R.id.radio_button_delivery_method_pickup:
+                    displayMessage(getString(R.string.delivery_method_pick_up));
+            }
+        }
+    }
+    public void displayMessage(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
     }
 }
