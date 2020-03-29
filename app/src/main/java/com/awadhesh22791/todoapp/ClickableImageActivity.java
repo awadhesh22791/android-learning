861332efd13d3830de9e66dbb70363273d30cb6d
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-29 19:21:21 +0530

    Added Image click event handler.

--- app/src/main/java/com/awadhesh22791/todoapp/ClickableImageActivity.java
+++ app/src/main/java/com/awadhesh22791/todoapp/ClickableImageActivity.java
@@ -3,6 +3,8 @@ package com.awadhesh22791.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ClickableImageActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ClickableImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clickable_image);
+    }
+    public void displayToastMessage(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        displayToastMessage(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view) {
+        displayToastMessage(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view) {
+        displayToastMessage(getString(R.string.froyo_order_message));
     }
 }
