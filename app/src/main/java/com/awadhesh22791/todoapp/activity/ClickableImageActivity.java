89f3d6c0f7021b297a1a299de1041c7d3202afb2
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-04-01 00:01:51 +0530

    Added items to menu.

--- app/src/main/java/com/awadhesh22791/todoapp/activity/ClickableImageActivity.java
+++ app/src/main/java/com/awadhesh22791/todoapp/activity/ClickableImageActivity.java
@@ -1,9 +1,12 @@
 package com.awadhesh22791.todoapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,7 +19,35 @@ public class ClickableImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clickable_image);
+        Toolbar myToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        switch (id){
+            case R.id.action_status:
+                Toast.makeText(getApplicationContext(),"You selected Status.",Toast.LENGTH_SHORT).show();
+                break;
+            case  R.id.action_favorites:
+                Toast.makeText(getApplicationContext(),"You selected Favorites.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(),"You selected Settings.",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_order:
+                Toast.makeText(getApplicationContext(),"You selected Orders.",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void displayToastMessage(String message){
         Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
     }
