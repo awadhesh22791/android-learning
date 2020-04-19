04859d0142b5f223d74b7cfc4c7854ba158edaa8
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-04-19 18:23:17 +0530

    Added android annotation plugin.

--- app/src/main/java/com/awadhesh22791/todoapp/activity/HelloToastActivity.java
+++ app/src/main/java/com/awadhesh22791/todoapp/activity/HelloToastActivity.java
@@ -9,22 +9,29 @@ import android.widget.Toast;
 
 import com.awadhesh22791.todoapp.R;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_hello_toast)
 public class HelloToastActivity extends AppCompatActivity {
 
     private int mCount=0;
+    @ViewById(R.id.show_count)
     TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_toast);
-        mShowCount=(TextView) findViewById(R.id.show_count);
     }
 
+    @Click(R.id.button_toast)
     public void showToast(View view) {
         Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show();
     }
 
+    @Click(R.id.button_count)
     public void countUp(View view) {
         mCount++;
         if(mShowCount!=null)
